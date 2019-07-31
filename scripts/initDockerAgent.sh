1acfb3a731900a87d55bda79698c1997a0a2efdb
Alok Prakash <alok.prakash@mmodal.com>
2019-07-31 14:20:40 -0400

    Passing the AWS region to agent config

--- scripts/initDockerAgent.sh
+++ scripts/initDockerAgent.sh
@@ -3,11 +3,11 @@ ROOT=/opt/licode
 SCRIPTS="$ROOT"/scripts
 
 setup_config() {
-  echo "Setting up config with " $1
+  echo "Setting up config with " $*
   cd $SCRIPTS/config_update
   source env/bin/activate
   #use the Secrets manager key name to get secrets
-  python3 configs.py $1 amazon
+  python3 configs.py $*
   deactivate
   cd $ROOT/scripts
 }
